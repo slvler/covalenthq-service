635328318da513240ed9e4738415af2921dcab1b
hs-qwerty <56368718+hs-qwerty@users.noreply.github.com>
2022-06-18 21:42:42 +0300

    initial command and registerPublishg method

---
+++ src/Console/FontCommand.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace qwerty\Fontawesome\Console;
+
+use Illuminate\Console\Command;
+
+class FontCommand extends Command
+{
+    protected $signature = "font:list";
+
+    protected $description = "font awesome font list";
+
+
+    public function handle()
+    {
+
+    }
+}
--- src/QwertyFontAwesomeServiceProvider.php
+++ src/QwertyFontAwesomeServiceProvider.php
@@ -9,16 +9,31 @@ class QwertyFontAwesomeServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerResources();
+        $this->registerPublishg();
+
+        if ($this->app->runningInConsole()){
+
+        }
     }
 
     public function register()
     {
-
+        $this->commands([
+            Console\FontCommand::class,
+        ]);
     }
 
     private function registerResources()
     {
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
     }
+
+    protected function registerPublishg()
+    {
+        $this->publishes([
+            __DIR__.'/../database/migrations/0000_00_00_000000_create_fonts_table.php' => database_path('migrations/0000_00_00_000000_create_fonts_table.php'),
+        ], 'migrations');
+    }
+
 
 }
