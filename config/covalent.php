1ba1ea831f3ed4ee39eb56d54d2786d58957bd1d
slvler <56368718+slvler@users.noreply.github.com>
2023-01-12 22:13:48 +0300

    Update covalent.php

--- config/covalent.php
+++ config/covalent.php
@@ -4,7 +4,7 @@ return [
 
     'covalent' => [
         'base_url' => 'https://api.covalenthq.com/',
-        'api_key' => 'ckey_53c8ac62a5a64e929bdacb88514',
+        'api_key' => 'XXXXXXXXXXXXXXXXXXXXXXX',
     ],
 
 ];
